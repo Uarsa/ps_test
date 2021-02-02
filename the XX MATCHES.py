6c1eba637b833c6243dcf00304aa3e7beab40d3f
Uarsa <59147934+Uarsa@users.noreply.github.com>
2021-02-02 20:57:46 +0300

    Create the XX MATCHES.py

---
+++ the XX MATCHES.py
@@ -0,0 +1,35 @@
+import random, time
+
+matches = 20
+while matches > 0:
+    m = int(input("YOU take matches: "))
+    if m > 3:
+        print("no more than three!")
+        print()
+        continue
+    
+    matches -= m
+    print("|" * matches, "x{} left".format(matches))
+    print()
+    
+    if matches == 0:
+        print("YOU WIN")
+        exit()
+    
+    time.sleep(1)
+    
+    if matches <= 3:
+        m1 = random.randint(1, matches)
+        print("COMPUTER take {} matches".format(m1))
+        matches -= m1
+        print("|" * matches, "x{} left".format(matches))
+        print()
+        
+    else:    
+        m1 = random.randint(1, 3)
+        print("COMPUTER take {} matches".format(m1))
+        matches -= m1
+        print("|" * matches, "x{} left".format(matches))
+        print()
+        
+print("COMPUTER WIN")  

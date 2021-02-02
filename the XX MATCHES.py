7756b2b4fd1e187b77b72e1a9022d0790f2db070
Uarsa <59147934+Uarsa@users.noreply.github.com>
2021-02-02 21:21:25 +0300

    Update the XX MATCHES.py

--- the XX MATCHES.py
+++ the XX MATCHES.py
@@ -1,37 +1,40 @@
 import random, time
 
+
 matches = 20
+print("x{} ".format(matches), "{}".format("|" * matches))
+print()
 while matches > 0:
     m = int(input("YOU take matches: "))
     if m > 3:
         print("no more than three!")
         print()
         continue
-    
+
     matches -= m
-    print("|" * matches, "x{} left".format(matches))
+    print("x{} ".format(matches), "{}".format("|" * matches))
     print()
-    
+
     if matches == 0:
         print("YOU WIN")
         exit()
-    
+
     time.sleep(1)
-    
+
     if matches <= 3:
-        m1 = random.randint(1, matches)
-        print("COMPUTER take {} matches".format(m1))
+        m1 = matches
+        print("COMPUTER takes {} matches".format(m1))
         matches -= m1
-        print("|" * matches, "x{} left".format(matches))
+        print("x{} ".format(matches), "{}".format("|" * matches))
         print()
-        
-    else:    
+
+    else:
         m1 = random.randint(1, 3)
-        print("COMPUTER take {} matches".format(m1))
+        print("COMPUTER takes {} matches".format(m1))
         matches -= m1
-        print("|" * matches, "x{} left".format(matches))
+        print("x{} ".format(matches), "{}".format("|" * matches))
         print()
-        
-print("COMPUTER WIN")  
+
+print("COMPUTER WIN")
 
 
